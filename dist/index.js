5520e12c886c2612d928d35ab7baf3d0f77ce9a7
heegu0311 <heegu0311@gmail.com>
2022-03-03 01:30:52 +0900

    Initial setup for this project

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+console.log(sayHi("Joseph Yoon", 34, "male"));
+//# sourceMappingURL=index.js.map
