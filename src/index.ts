4a18f11211e20687da2d574b3358cfbbf5276e56
heegu0311 <heegu0311@gmail.com>
2022-03-03 01:45:04 +0900

    Learned Basics of Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const myFriend = new Human("PSM", 33, "male");
+
+const person = {
+  name: "Joseph",
+  gender: "male",
+  age: 34,
 };
 
-console.log(sayHi("Joseph Yoon", 34, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(myFriend));
